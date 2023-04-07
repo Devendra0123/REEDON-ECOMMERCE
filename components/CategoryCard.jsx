e5a9b96b0c56e1005f27d59a8a195fe0bf02cbab
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2023-04-07 07:39:05 +0545

    Update and rename CategoryCard.tsx to CategoryCard.jsx

--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -22,7 +22,7 @@ export default async function CategoryCard(){
   return (
     <div className='w-full bg-slate-500 p-[20px] pt-[40px] flex justify-center items-center flex-wrap gap-[30px] lg:gap-[50px] mt-[0px]'>
 {
-   categories && categories.map((item : any,index : number)=>(
+   categories && categories.map((item,index)=>(
         <Link href={`/product/category?category_id=${item.slug.current}`} key={index} className='z-10 relative'>
             <p className='font-medium text-lg bg-gradient-to-r from-yellow-500 to-transparent p-[5px] rounded-t-lg'>
                 {item.categoryName}
