3573c87b4756c53c789ca2bbf007f34e0b023fca
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2023-04-07 09:22:32 +0545

    Update Category.tsx

--- components/Category.tsx
+++ components/Category.tsx
@@ -21,7 +21,7 @@ await client.fetch(query, { next: { revalidate: 60 } }).then(res => setCategorie
     },[])
 
   return (
-      <div className='hidden lg:block fixed top-[170px] left-[20px] flex flex-col gap-[10px] bg-slate-800 p-[20px]'>
+      <div className='hidden lg:block fixed top-[200px] left-[20px] flex flex-col gap-[10px] bg-slate-800 p-[20px]'>
         {categories && categories.map((item : any, index : number) => (
           <Link href={`/product/category?category_id=${item.slug.current}`} key={index}>
             <p className='p-[5px] font-poppins font-medium text-white text-lg'>
