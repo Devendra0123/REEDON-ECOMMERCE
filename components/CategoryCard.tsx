63735a188c90b75267269e9c0a8f84f496f02f0f
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2023-04-07 07:32:39 +0545

    Update CategoryCard.tsx

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,12 +1,9 @@
-'use client'
+
 import React,{useState, useEffect} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { client, urlFor } from '@/utils/sanityClient'
 
-const CategoryCard = () => {
-
-    const [categories, setCategories] = useState<any>()
     const fetchCategories = async () => {
     const query = `*[_type == "category"]{
       _id,
@@ -15,12 +12,12 @@ const CategoryCard = () => {
       slug
   }`
   
-await client.fetch(query, { next: { revalidate: 60 } }).then(res => setCategories(res));
- 
+   const response = await client.fetch(query, { next: { revalidate: 60 } }).then(res => res);
+ return response;
   }
-    useEffect(()=>{
-    fetchCategories();
-    },[])
+export default async function CategoryCard(){
+
+   const categories = await fetchCategories()
 
   return (
     <div className='w-full bg-slate-500 p-[20px] pt-[40px] flex justify-center items-center flex-wrap gap-[30px] lg:gap-[50px] mt-[0px]'>
