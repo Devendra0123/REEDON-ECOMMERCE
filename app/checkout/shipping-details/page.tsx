c042596b306044062e20495ac82cc24828b812bd
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2023-04-07 22:24:55 +0545

    Update page.tsx

--- app/checkout/shipping-details/page.tsx
+++ app/checkout/shipping-details/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const ShippingDetail = () => {
   return (
-    <div>
+    <div className='max-w-screen'>
         <Navbar />
         <ShippingDetails />
         <Footer />
